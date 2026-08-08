d71df3ea8830c245f4b129485355c3eecbeafa06
Divyansh Singh <divyansh222f@gmail.com>
2026-08-09 04:24:04 +0530

    Remove temporary 500 debug handler

--- CollegeERP/urls.py
+++ CollegeERP/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-handler500 = 'CollegeERP.debug500.debug500'
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
