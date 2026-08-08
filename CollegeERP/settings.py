fae4155b951ed7336a490166e1e87ec3923b3131
Divyansh Singh <divyansh222f@gmail.com>
2026-08-09 03:27:30 +0530

    Fix empty DJANGO_SECRET_KEY env var causing ImproperlyConfigured error

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'jy8c-n9y=pf##!2^jae-l_5iafq6q%wfq8gdb6c0r5d52su+9y')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or 'jy8c-n9y=pf##!2^jae-l_5iafq6q%wfq8gdb6c0r5d52su+9y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
